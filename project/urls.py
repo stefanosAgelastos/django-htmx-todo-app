65bb0ae662d7b2f94b0e13e0f95bec8dd225c107
Stefanos <sagelastos@gmail.com>
2022-04-06 22:25:08 +0200

    polls index sample

--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('todo/', include('todo.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
