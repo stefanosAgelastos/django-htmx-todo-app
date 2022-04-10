01fb9b004c06646b9a37e8b33e19c989dd87e4ea
Stefanos <sagelastos@gmail.com>
2022-04-10 20:29:07 +0200

    added rerank and delete in todo model

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from dataclasses import field, fields
+from django.db import models, transaction
 
 
 class Todo(models.Model):
@@ -9,6 +10,9 @@ class Todo(models.Model):
     class Meta:
         ordering = ['rank']
 
+    def __str__(self):
+        return f'({self.rank}) - {self.pk} - {self.title} - {self.text}'
+
     @classmethod
     def create(cls, title=title, text=text):
         cls.objects.create(
@@ -16,9 +20,32 @@ class Todo(models.Model):
             text=text,
             rank=cls.highest_rank + 1)
 
+    def delete(self, *args, **kwargs):
+        self.rerank(type(self).highest_rank)
+        super(type(self), self).delete(*args, **kwargs)
+
     @classmethod
     @property
     def highest_rank(cls):
-        return int(cls.objects.all().aggregate(models.Max('rank'))['rank__max'] or 1)
+        return int(cls.objects.all().aggregate(models.Max('rank'))['rank__max'] or 0)
 
-    # def rerank(self, new_rank):
+    def rerank(self, new_rank):
+        with transaction.atomic():
+            if self.rank == new_rank:
+                return
+            elif new_rank < self.rank:
+                delta = 1
+                todos = type(self).objects.filter(
+                    rank__gte=new_rank).filter(rank__lte=self.rank)
+            else:
+                delta = -1
+                todos = type(self).objects.filter(
+                    rank__gte=self.rank).filter(rank__lte=new_rank)
+
+            for todo in todos:
+                if todo.rank == self.rank:
+                    todo.rank = new_rank
+                else:
+                    todo.rank += delta
+
+            type(self).objects.bulk_update(todos, fields=['rank'])
