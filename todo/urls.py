09a07b3f9a135579106fa2e91cbafd9b280a5acf
Stefanos <sagelastos@gmail.com>
2022-04-12 16:43:54 +0200

    work on todo details and other partials

--- todo/urls.py
+++ todo/urls.py
@@ -6,6 +6,10 @@ app_name = 'todo'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('todo_list_partial', views.todo_list_partial, name='todo_list_partial'),
+    path('todo_details_partial/<int:pk>',
+         views.todo_details_partial, name='todo_details_partial'),
+    path('create', views.create, name='create'),
     # path('api/v1', TodoList.as_view()),
     # path('api/v1/<int:pk>', TodoDetail.as_view()),
     # path('api/v1/rest-auth', include('rest_framework.urls')),
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from cgitb import text
+from crypt import methods
+from django.shortcuts import get_object_or_404, render
+from .models import Todo
 
 
 def index(request):
     return render(request, 'todo/index.html', {})
+
+
+def todo_list_partial(request):
+    todos = Todo.objects.all()
+    return render(request, 'todo/todo_list_partial.html', {'todos': todos})
+
+
+def create(request):
+    Todo.create(title=request.POST['title'], text=request.POST['text'])
+    response = render(request, 'todo/index.html', {})
+    response['HX-Redirect'] = request.META['HTTP_HX_CURRENT_URL']
+    return response
+
+
+def todo_details_partial(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+
+    if request.method == 'GET':
+        return render(request, 'todo/todo_details_partial.html', {'todo': todo})
+    elif request.method == 'DELETE':
+        todo.delete()
+    elif request.method == 'PUT':
+        # update item in DB
+        ...
+
+    todos = Todo.objects.all()
+    return render(request, 'todo/todo_list_partial.html', {'todos': todos})
